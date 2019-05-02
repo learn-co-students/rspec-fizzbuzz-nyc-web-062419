a05911ced115229cb74863e0670c274371b80de7
Luis Ascencio <luismiguelascencio@gmail.com>
2019-05-02 14:46:28 -0400

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,18 @@
+def fizzbuzz(num1)
+
+    if num1 % 15 ==0
+        "FizzBuzz"
+
+    elsif num1 % 3== 0
+        "Fizz"
+    elsif num1 % 5 ==0
+    "Buzz"
+
+
+end 
+end
+
+
+
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
